14ae8676cc178f3bd0947db0472f907342f9a1c9
ivanzujeu <ivanzujeu@gmail.com>
2020-02-13 10:28:13 +0300

    Initial Commit

---
+++ Zuev_Zadanie0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zuev_Zadanie0
+//
+//  Created by Ivan on 2/13/20.
+//  Copyright © 2020 Ivan Zuev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
